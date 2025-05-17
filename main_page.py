dd47d05aba3a7ed2e0201ba03ef7882871efd0d2
M Farrel Danendra Rachim <13521048@std.stei.itb.ac.id>
2025-05-17 21:54:52 +0700

    feat: add initial streamlit page

---
+++ main_page.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import plotly.express as px
+import plotly.graph_objects as go
+import pandas as pd
+import warnings
+warnings.filterwarnings('ignore')
+
+st.set_page_config(page_title="Analysis of U.S. Flight Data (2013-2023)", layout="wide")
