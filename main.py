28852ca7b793dd173396cd74c03a2958beeeba03
M Farrel Danendra Rachim <13521048@std.stei.itb.ac.id>
2025-05-28 13:46:56 +0700

    feat: remove desc

--- main.py
+++ main.py
@@ -16,20 +16,9 @@ st.set_page_config(page_title="Analysis of U.S. Flight Data (2013-2023)", layout
 st.markdown(
     """
     <div style='
-        border: 2px solid #CCCCCC;
-        border-radius: 10px;
-        padding: 10px;
-        margin-bottom: 10px;
     '>
         <h1 style='margin-top: 0;'>✈️ US Airline Flight Delay Dashboard</h1>
-        <p style='font-size:17px; margin-bottom: 15px;'>
-            United States is one of the busiest countries in the world when it comes to air travel services.
-            Every year, millions of flights are operated by various carriers across numerous airports throughout the country.
-            With such a high volume of air traffic, it is not uncommon for the U.S. aviation system to face challenges,
-            particularly those related to flight delays.
-            <br><br>
-            To gain a deeper understanding of the flight delays situation in the United States, let’s explore the following data.
-        </p>
+        
     </div>
     """,
     unsafe_allow_html=True
